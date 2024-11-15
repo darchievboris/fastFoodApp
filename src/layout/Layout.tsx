5576200d9a920e7b0c1fda7dff18f0b9ff39fef9
darchievboris <darchievboris@gmail.com>
2024-11-15 12:54:15 +0300

    made sidebire in layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,37 @@
 import {Link, Outlet} from "react-router-dom";
+import styles from './Layout.module.css'
+import Button from "../components/UI/Button/Button.tsx";
 
 const Layout = ({}) => {
     return (
-        <>
-            <Link to='/'>Menu</Link>
-            <Link to='/Cart'>Cart</Link>
-            <Outlet/>
-        </>
+        <div className={styles.layout}>
+            <div className={styles.sidebar}>
+                <div className={styles.user}>
+                    <img src="Avatar.png" className={styles.avatar} alt="Аватар пользователя"/>
+                    <div className={styles.name}>Антон Ларичев</div>
+                    <div className={styles.email}>alaricode@ya.ru</div>
+                </div>
+                <div className={styles.menu}>
+                    <Link to='/' className={styles.link}>
+                        <img src="menu-icon.svg" alt="Иконка меню"/>
+                        Menu
+                    </Link>
+                    <Link to='/Cart' className={styles.link}>
+                        <img src="cart-icon.svg" alt="Иконка корзины"/>
+                        Cart
+                    </Link>
+                </div>
+
+                <Button className={styles.exit}>
+                    <img src="exit.svg" alt="Иконка выхода"/>
+                    Cart
+                </Button>
+
+            </div>
+            <div className={styles.content}>
+                <Outlet/>
+            </div>
+        </div>
     );
 };
 
